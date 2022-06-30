4b6b5408e48ffe01cd25a6e81b2ace0cc125e2a1
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-30 16:02:43 +0200

    Create cs.py

---
+++ translations/cs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# Czech translations of vikdevelop/apm
+# AUTHOR: vikdevelop <https://github.com/vikdevelop>
+
+# Help section
+help_install_appimages = "Nainstalovat AppImage"
+help_uninstall_appimages = "Odinstalovat AppImage"
+help_see_all_appimages = "Zobrazit všechny nainstalované AppImage"
+help_search_appimages = "Hledat AppImage v repositáři"
+help_run_appimages = "Spustit nainstalovaný AppImage"
+
+# General strings
+continue_input = "Chcete pokračovat?"
+canceled = "Zrušeno."
+to_text = " do "
+done_text = "Hotovo!"
+package = "Balíček "
+type_apm_help = "' nebo napište apm --help"
+
+# Installation section
+installation_appimage_title = "Instalace AppImage"
+name_tab = "Název"
+version_tab = "Verze"
+source_tab = "Zdroj"
+downloading_status = "Stahování "
+installing_text = "Instalace "
+creating_moving_text = "Vytváření a přesouvání spustitelného souboru "
+
+# Uninstallation section
+package_is_not_installed = " není nainstalovaný. Zkuste ho nainstalovat pomocí 'apm -install "
+chacking_installation_status = "Kontrola stavu instalace balíčku "
+uninstallation_appimage = "Odinstalace AppImage"
+date_tab = "Datum instalace"
+path_tab = "Umíštění"
+removing_text = "Odstraňování balíčku "
+from_dir_text = " z "
+desktop_file = " spustitelného souboru "
+
+# Search section
+search_results = "Výsledky vyhledávání"
+repofile_tab = "Soubor repositáře"
+
+# See all installed AppImages section
+installed_appimages_text = "Toto jsou všechny AppImage nainstalované na vašem počítači:"
