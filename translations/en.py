afc31ae9c6a4f319c03c19307e4acf386c6f6d76
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-30 16:02:19 +0200

    Add English translations

---
+++ translations/en.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# English translations of vikdevelop/apm
+# AUTHOR: vikdevelop <https://github.com/vikdevelop>
+
+# Help section
+help_install_appimages = "Install AppImages"
+help_uninstall_appimages = "Uninstall AppImages"
+help_see_all_appimages = "See all installed AppImages"
+help_search_appimages = "Search AppImages in repository"
+help_run_appimages = "Run installed AppImages"
+
+# General strings
+continue_input = "Would you like to continue?"
+canceled = "Canceled."
+to_text = " to "
+done_text = "Done!"
+package = "Package "
+type_apm_help = "' or type apm --help"
+
+# Installation section
+installation_appimage_title = "Installation AppImage"
+name_tab = "Name"
+version_tab = "Version"
+source_tab = "Source"
+downloading_status = "Downloading "
+installing_text = "Installing "
+creating_moving_text = "Creating and moving desktop file "
+
+# Uninstallation section
+package_is_not_installed = " is not installed. Try install with 'apm -install "
+chacking_installation_status = "Checking installation status of AppImage "
+uninstallation_appimage = "Uninstallation AppImage"
+date_tab = "Installation date"
+path_tab = "Path"
+removing_text = "Uninstallation package "
+from_dir_text = " from "
+desktop_file = " desktop file "
+
+# Search section
+search_results = "Search results"
+repofile_tab = "Repository file"
+
+# See all installed AppImages section
+installed_appimages_text = "This is all AppImages installed on your computer:"
