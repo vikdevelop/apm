79b50bc2f174ff449c9ac1a5d774d4d299c0d65a
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-18 19:14:56 +0200

    Changed directory for building
    From: /tmp to ~/.cache

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/sh
-git clone https://github.com/vikdevelop/apm /tmp/apm > /dev/null 2>&1
-cd /tmp/apm
+ #!/usr/bin/sh
+git clone https://github.com/vikdevelop/apm ~/.cache/apm > /dev/null 2>&1
+cd ~/.cache/apm
 echo "Installing depency runtime and SDK"
 flatpak install runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 org.flatpak.Builder -y > /dev/null 2>&1
 echo "Building apm with flatpak builder"
-flatpak run org.flatpak.Builder build flatpak.yml --install --user > /dev/null 2>&1
+flatpak run org.flatpak.Builder build flatpak.yml --install --user
 python3 flatpak.py
 echo "Done! Try run APM with command: 'apm --help' or 'flatpak run com.github.vikdevelop.apm --help'"
