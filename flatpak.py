2c965c49848b9840e1c661858acb5403440361a8
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-18 18:18:59 +0200

    Update flatpak.py

--- flatpak.py
+++ flatpak.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
-H = os.path.expanduser('~')
+HOME = os.path.expanduser('~')
 with open('%s/.bashrc' % HOME, 'a') as b:
   b.write('\nalias apm="flatpak run com.github.vikdevelop.apm"')
 with open('%s/.zshrc' % HOME, 'a') as z:
