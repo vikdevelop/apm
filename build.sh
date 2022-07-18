4f67f224dc1e91c9025a4d74da3a7865a3c2befd
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-18 18:24:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 git clone https://github.com/vikdevelop/apm /tmp/apm > /dev/null 2>&1
 cd /tmp/apm
 echo "Installing depency runtime and SDK"
-flatpak install runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 -y > /dev/null 2>&1
-echo "Building apm"
-flatpak-builder build flatpak.yml --install --user > /dev/null 2>&1
+flatpak install runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 org.flatpak.Builder -y > /dev/null 2>&1
+echo "Building apm with flatpak builder"
+flatpak run org.flatpak.Builder build flatpak.yml --install --user > /dev/null 2>&1
 python3 flatpak.py
 echo "Done! Try run APM with command: 'apm --help' or 'flatpak run com.github.vikdevelop.apm --help'"
