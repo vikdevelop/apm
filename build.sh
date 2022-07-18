9ef8f706a68e24c787b884ccdf3950cae9f23217
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-18 19:23:43 +0200

    Hide output of command flatpak-builder

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd ~/.cache/apm
 echo "Installing depency runtime and SDK"
 flatpak install runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 org.flatpak.Builder -y > /dev/null 2>&1
 echo "Building apm with flatpak builder"
-flatpak run org.flatpak.Builder build flatpak.yml --install --user
+flatpak run org.flatpak.Builder build flatpak.yml --install --user > /dev/null 2>&1
 python3 flatpak.py
 echo "Done! Try run APM with command: 'apm --help' or 'flatpak run com.github.vikdevelop.apm --help'"
