3771cbca51ea89fc3a112ea306890d9da0eabe9a
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-18 18:03:21 +0200

    Create flatpak.py

---
+++ flatpak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+H = os.path.expanduser('~')
+with open('%s/.bashrc' % HOME, 'a') as b:
+  b.write('\nalias apm="flatpak run com.github.vikdevelop.apm"')
+with open('%s/.zshrc' % HOME, 'a') as z:
+  z.write('\nalias apm="flatpak run com.github.vikdevelop.apm"')
