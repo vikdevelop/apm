7af1f3606174ef5b14a61786b46e243be53a8f06
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-20 19:56:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git clone https://github.com/vikdevelop/apm ~/.cache/apm > /dev/null 2>&1
 cd ~/.cache/apm
 echo "Installing depency runtime and SDK"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 org.flatpak.Builder -y > /dev/null 2>&1
+flatpak install --system runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 org.flatpak.Builder -y > /dev/null 2>&1
 echo "Building apm with flatpak builder"
 flatpak run org.flatpak.Builder build flatpak.yml --install --user > /dev/null 2>&1
 python3 flatpak.py
