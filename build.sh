128c1c85fd2c3127332a39eafe82e0a15d7213ba
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-08-02 10:54:16 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install --system runtime/org.freedesktop.Platform/x86_64/21.08 runtime/org.freedesktop.Sdk/x86_64/21.08 org.flatpak.Builder -y > /dev/null 2>&1
 echo "Building apm with flatpak builder"
 flatpak run org.flatpak.Builder build flatpak.yml --install --user > /dev/null 2>&1
-python3 flatpak.py
+python3 aliases.py
 echo "Done! Try run APM with command: 'apm --help' or 'flatpak run com.github.vikdevelop.apm --help'"
